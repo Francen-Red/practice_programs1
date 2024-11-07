47f34c667da27b3c839386946aa9e69ac66fe37a
Francen-Red <francenvenissered@gmail.com>
2024-11-08 04:23:49 +0800

    Print equal if 2 inputted numbers are equal

---
+++ 3rd.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        num_1 = int(input("Please input a number: "))
+        break
+    except:
+        print("Error! Please input a valid number")
+
+while True:
+    try:
+        num_2 = int(input("Please input a number: "))
+        break
+    except:
+        print("Error! Please input a valid number")
+
+if num_1 ==  num_2:
+    print("Equal")
