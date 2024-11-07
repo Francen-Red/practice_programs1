5b953f024edb7c7eed30ba3a3a6310b06600230a
Francen-Red <francenvenissered@gmail.com>
2024-11-08 07:32:48 +0800

    Print the sum of 10 inputted the numbers

---
+++ 5th.py
@@ -0,0 +1,25 @@
+def get_10_numbers(number_input):
+    while True:
+        try:
+            return int(input(number_input))
+        except:
+            print("Error! Please input a valid number.")
+
+num_1 = get_10_numbers("Please input a number: ")
+num_2 = get_10_numbers("Please input a number: ")
+num_3 = get_10_numbers("Please input a number: ")
+num_4 = get_10_numbers("Please input a number: ")
+num_5 = get_10_numbers("Please input a number: ")
+num_6 = get_10_numbers("Please input a number: ")
+num_7 = get_10_numbers("Please input a number: ")
+num_8 = get_10_numbers("Please input a number: ")
+num_9 = get_10_numbers("Please input a number: ")
+num_10 = get_10_numbers("Please input a number: ")
+
+
+total_sum = (num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8 + num_9 + num_10)
+print("The sum is:", total_sum)
+
+
+
+
