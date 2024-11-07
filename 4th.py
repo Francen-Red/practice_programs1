d4a1062319d9e33a50265907bc2640de52ead9ce
Francen-Red <francenvenissered@gmail.com>
2024-11-08 04:35:46 +0800

    Print the difference between 2 inputted numbers

---
+++ 4th.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        num_1 = int(input("Please input a number: "))
+        break
+    except:
+        print("Error! Please input a valid number")
+
+while True:
+    try:
+        num_2 = int(input("Please input another number: "))
+        break
+    except:
+        print("Error! Please input a valid number")
+
+print("The difference is:", num_1 - num_2)
